8ee0778ff43bdc7a37378a631491247c2b8df21a
Gustavo de Paula <gdepaula@gmail.com>
2023-11-28 12:04:33 -0300

    Interpolation

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -10,56 +10,9 @@ import {
 
 @Component({
   selector: 'app-root',
-  template: `<app-title title="Olá Mundo" *ngIf="destruir"></app-title>
-    {{ valor }}
-    <button (click)="adicionar()">Adicionar</button>
-    <button (click)="destruirComponente()">Destruir componente</button>
-    <router-outlet></router-outlet>`,
+  template: `<app-data-binding></app-data-binding
+    ><router-outlet></router-outlet>`,
 })
-export class AppComponent
-  implements
-    OnInit,
-    DoCheck,
-    AfterContentInit,
-    AfterContentChecked,
-    AfterViewInit,
-    AfterViewChecked
-{
-  public valor: number = 1;
-
-  public destruir: boolean = true;
-
-  public adicionar(): number {
-    return (this.valor += 1);
-  }
-
-  public destruirComponente() {
-    this.destruir = false;
-  }
-
-  ngOnInit(): void {
-    setTimeout(() => {
-      console.log(1);
-    }, 5000);
-  }
-
-  ngDoCheck(): void {
-    console.log('ngDoCheck');
-  }
-
-  ngAfterContentInit(): void {
-    console.log('ngAfterContentInit');
-  }
-
-  ngAfterViewChecked(): void {
-    console.log('ngAfterViewChecked');
-  }
-
-  ngAfterViewInit(): void {
-    console.log('ngAfterViewInit');
-  }
-
-  ngAfterContentChecked(): void {
-    console.log('ngAfterContentChecked');
-  }
+export class AppComponent implements OnInit {
+  ngOnInit(): void {}
 }
