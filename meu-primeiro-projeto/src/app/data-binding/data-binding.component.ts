038fdbaf9c3133484c850f1d2c149473edf0e92b
Gustavo de Paula <gdepaula@gmail.com>
2023-12-01 14:35:19 -0300

    Event Binding

--- meu-primeiro-projeto/src/app/data-binding/data-binding.component.ts
+++ meu-primeiro-projeto/src/app/data-binding/data-binding.component.ts
@@ -8,9 +8,22 @@ import { Component } from '@angular/core';
 export class DataBindingComponent {
   public nome: string = 'Gusta';
   public idade: number = 40;
+  public position: { x: number; y: number } = { x: 0, y: 0 };
 
   public checkedDisable: boolean = true;
+
   public imgSrc: string =
     'https://plugins.shopware-staging.overdose.digital/media/ca/a7/df/1692086257/testing.jpeg';
+
   public imgTitle: string = 'Teste';
+
+  public alertInfo(valor: MouseEvent) {
+    console.log(valor);
+  }
+
+  public mouseMoveTeste(valor: MouseEvent) {
+    // console.log(valor);
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
 }
