053cf369c5096283d6323ad07ff3dcf9e5dc587c
Gustavo de Paula <gdepaula@gmail.com>
2023-12-07 11:14:21 -0300

    Pipe

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -10,9 +10,7 @@ import {
 
 @Component({
   selector: 'app-root',
-  template: `<app-diretivas-atributos>
-      <h1>Aulas de diretiva atributo</h1>
-      <hr /></app-diretivas-atributos
+  template: `<app-diretivas-atributos></app-diretivas-atributos
     ><router-outlet></router-outlet>`,
 })
 export class AppComponent implements OnInit {
