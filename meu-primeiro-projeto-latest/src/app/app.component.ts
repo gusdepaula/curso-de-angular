498768575dc4c486f5e84a8b8c6569c2c9971e9e
Gustavo de Paula <gdepaula@gmail.com>
2023-12-08 15:26:02 -0300

    Entendendo o componente

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // templateUrl: './app.component.html',
+  template: `<h1>Curso de Angular</h1>
+    <h2>Components</h2> `,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'meu-primeiro-projeto-latest';
