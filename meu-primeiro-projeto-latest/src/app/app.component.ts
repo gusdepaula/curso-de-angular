fa75a9b14b4c59716e112b72f6893b226714de7a
Gustavo de Paula <gdepaula@gmail.com>
2023-12-12 11:14:54 -0300

    Text interpolation

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -2,15 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
+import { TemplateBindingComponent } from './components/template-binding/template-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent],
+  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent],
   template: `<h1>Curso de Angular</h1>
-    <div class="theme-red">
-      <app-new-component />
-    </div>`,
-  styleUrl: './app.component.scss',
+    <app-template-binding />`,
 })
 export class AppComponent {}
