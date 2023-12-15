306055a0fd87572adaacfc7da34687ebb503d16a
Gustavo de Paula <gdepaula@gmail.com>
2023-12-15 11:46:46 -0300

    Template Variables

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NewComponent } from './components/new-component/new-component.component';
-import { TemplateBindingComponent } from './components/template-binding/template-binding.component';
+import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent],
+  imports: [CommonModule, RouterOutlet, TemplateVariablesComponent],
   template: `<h1>Curso de Angular</h1>
-    <app-template-binding />`,
+    <app-template-variables />`,
 })
 export class AppComponent {}
