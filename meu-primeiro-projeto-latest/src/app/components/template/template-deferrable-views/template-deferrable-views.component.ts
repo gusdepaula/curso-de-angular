aca94999ca941a7719272be199a4a7d62f7ebf7f
Gustavo de Paula <gdepaula@gmail.com>
2023-12-21 13:35:55 -0300

    @defer - when

--- meu-primeiro-projeto-latest/src/app/components/template/template-deferrable-views/template-deferrable-views.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-deferrable-views/template-deferrable-views.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NewComponent } from '../../new-component/new-component.component';
+import { Observable, delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-template-deferrable-views',
@@ -9,4 +10,12 @@ import { NewComponent } from '../../new-component/new-component.component';
   templateUrl: './template-deferrable-views.component.html',
   styleUrl: './template-deferrable-views.component.scss',
 })
-export class TemplateDeferrableViewsComponent {}
+export class TemplateDeferrableViewsComponent {
+  public isTrue = false;
+
+  public loadingData$: Observable<string[]> = of([
+    'item 1',
+    'item 2',
+    'item 3',
+  ]).pipe(delay(5000));
+}
