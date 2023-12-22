ac5bda74eaf4e0531f296c7f8279f7efbacc1a3a
Gustavo de Paula <gdepaula@gmail.com>
2023-12-22 10:33:11 -0300

    Usando o Signal

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { TemplateDeferrableViewsComponent } from './components/template/template-deferrable-views/template-deferrable-views.component';
+import { SignalsComponent } from './components/signals/signals.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TemplateDeferrableViewsComponent],
+  imports: [CommonModule, RouterOutlet, SignalsComponent],
   template: `<h1>Curso de Angular</h1>
-    <app-template-deferrable-views /> `,
+    <app-signals /> `,
 })
 export class AppComponent {}
---
+++ meu-primeiro-projeto-latest/src/app/components/signals/signals.component.ts
@@ -0,0 +1,31 @@
+import { Component, computed, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  standalone: true,
+  imports: [],
+  templateUrl: './signals.component.html',
+  styleUrl: './signals.component.scss',
+})
+export class SignalsComponent {
+  public firstName = signal('Gustavo');
+  public lastName = signal('de Paula');
+
+  public fullName = computed(() => {
+    return this.firstName() + ' ' + this.lastName();
+  });
+
+  public array = signal([1]);
+
+  constructor() {}
+
+  public updateName() {
+    return this.firstName.set('João');
+  }
+
+  public updateArray() {
+    this.array.update((oldValue: Array<number>) => {
+      return [...oldValue, oldValue.length + 1];
+    });
+  }
+}
