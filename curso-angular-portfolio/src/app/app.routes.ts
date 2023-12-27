da3f8a44d9f2666fb1011a40493260ba7a2178c6
Gustavo de Paula <gdepaula@gmail.com>
2023-12-27 14:08:37 -0300

    Header Component

--- curso-angular-portfolio/src/app/app.routes.ts
+++ curso-angular-portfolio/src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './modules/portfolio/pages/home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
