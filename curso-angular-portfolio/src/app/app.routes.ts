cc19eeb237ce59023677fd54c7d5866582fb087f
Gustavo de Paula <gdepaula@gmail.com>
2023-12-27 13:50:43 -0300

    Passos iniciais do projeto prático: Portfólio

---
+++ curso-angular-portfolio/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './modules/portfolio/pages/home/home.component';
+
+const routes: Routes = [{ path: '', component: HomeComponent }];
