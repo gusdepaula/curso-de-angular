836c2940b3f6874ffd5d1bad9fdb79dadd193541
Gustavo de Paula <gdepaula@gmail.com>
2024-01-05 14:24:03 -0300

    [ DIALOG ] Projects Component

--- curso-angular-portfolio/src/app/modules/portfolio/components/projects/projects.component.ts
+++ curso-angular-portfolio/src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,16 +1,20 @@
 import { Component, inject, signal } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 // Interface
 import { IProjects } from '../../interface/IProjects.interface';
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
   public arrayProjects = signal<IProjects[]>([
     {
       src: 'assets/img/projects/vfull_.jpeg',
@@ -43,4 +47,11 @@ export class ProjectsComponent {
       ],
     },
   ]);
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS,
+    });
+  }
 }
