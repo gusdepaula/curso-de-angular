07318eb392437a72dea1d6b1684498c7abbae37f
Gustavo de Paula <gdepaula@gmail.com>
2024-01-08 09:53:40 -0300

    @Input

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SignalsComponent } from './components/signals/signals.component';
+import { PaiOuMaeComponent } from './components/comunicacao-entre-components/pai-ou-mae/pai-ou-mae.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SignalsComponent],
+  imports: [CommonModule, RouterOutlet, PaiOuMaeComponent],
   template: `<h1>Curso de Angular</h1>
-    <app-signals /> `,
+    <app-pai-ou-mae /> `,
 })
 export class AppComponent {}
