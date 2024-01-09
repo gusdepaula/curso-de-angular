a7d5f944503049917024634be2920dc12f178bcb
Gustavo de Paula <gdepaula@gmail.com>
2024-01-09 11:07:34 -0300

    Pipes continuação

--- meu-primeiro-projeto-latest/src/app/components/pipes/angular-pipes/angular-pipes.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/pipes/angular-pipes/angular-pipes.component.ts
@@ -1,19 +1,36 @@
 import {
   AsyncPipe,
+  CurrencyPipe,
   DatePipe,
+  DecimalPipe,
   JsonPipe,
   LowerCasePipe,
+  PercentPipe,
   UpperCasePipe,
+  registerLocaleData,
 } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, LOCALE_ID, signal } from '@angular/core';
 import { Observable, delay, of } from 'rxjs';
+
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
 
 @Component({
   selector: 'app-angular-pipes',
   standalone: true,
-  imports: [DatePipe, UpperCasePipe, LowerCasePipe, JsonPipe, AsyncPipe],
+  imports: [
+    DatePipe,
+    UpperCasePipe,
+    LowerCasePipe,
+    JsonPipe,
+    AsyncPipe,
+    CurrencyPipe,
+    DecimalPipe,
+    PercentPipe,
+  ],
   templateUrl: './angular-pipes.component.html',
   styleUrl: './angular-pipes.component.scss',
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
 })
 export class AngularPipesComponent {
   public date = signal(new Date());
