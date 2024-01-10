58e9768aa43d528138676eb37d3b0725f0168790
Gustavo de Paula <gdepaula@gmail.com>
2024-01-10 10:58:25 -0300

    Pipes com Locale Global

--- meu-primeiro-projeto-latest/src/app/components/pipes/angular-pipes/angular-pipes.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/pipes/angular-pipes/angular-pipes.component.ts
@@ -7,13 +7,9 @@ import {
   LowerCasePipe,
   PercentPipe,
   UpperCasePipe,
-  registerLocaleData,
 } from '@angular/common';
 import { Component, LOCALE_ID, signal } from '@angular/core';
 import { Observable, delay, of } from 'rxjs';
-
-import localePt from '@angular/common/locales/pt';
-registerLocaleData(localePt);
 
 @Component({
   selector: 'app-angular-pipes',
@@ -30,7 +26,6 @@ registerLocaleData(localePt);
   ],
   templateUrl: './angular-pipes.component.html',
   styleUrl: './angular-pipes.component.scss',
-  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
 })
 export class AngularPipesComponent {
   public date = signal(new Date());
