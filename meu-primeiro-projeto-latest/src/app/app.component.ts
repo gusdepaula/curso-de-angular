d7a5b279f0bc0e2d4e47cdb8859b58028326ab5a
Gustavo de Paula <gdepaula@gmail.com>
2024-01-11 10:45:47 -0300

    Template driven forms - Introdução

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { AngularPipesComponent } from './components/pipes/angular-pipes/angular-pipes.component';
+import { TemplateDrivenFormsComponent } from './components/forms/template-driven-forms/template-driven-forms.component';
+import { ReactiveFormsComponent } from './components/forms/reactive-forms/reactive-forms.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AngularPipesComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    TemplateDrivenFormsComponent,
+    ReactiveFormsComponent,
+  ],
   template: `<h1>Curso de Angular</h1>
-    <app-angular-pipes />`,
+    <app-template-driven-forms />
+    <app-reactive-forms /> `,
 })
 export class AppComponent {}
