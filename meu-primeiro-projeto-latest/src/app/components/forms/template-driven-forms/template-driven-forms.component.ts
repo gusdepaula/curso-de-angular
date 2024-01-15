7ba42155a0a76f2a5c0b7fb98d5356186d8f8b9f
Gustavo de Paula <gdepaula@gmail.com>
2024-01-15 09:52:42 -0300

    Form Select

--- meu-primeiro-projeto-latest/src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-forms',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './template-driven-forms.component.html',
   styleUrl: './template-driven-forms.component.scss',
 })
-export class TemplateDrivenFormsComponent {}
+export class TemplateDrivenFormsComponent {
+  public listComidas = signal<Array<{ comida: string; preco: string }>>([
+    {
+      comida: 'X-salada',
+      preco: 'R$ 10',
+    },
+    {
+      comida: 'X-bacon',
+      preco: 'R$ 11',
+    },
+    {
+      comida: 'Coxinha',
+      preco: 'R$ 6',
+    },
+  ]);
+}
