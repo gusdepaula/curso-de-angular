3d42f7e73312e831aa9004b4f606d37abab0a236
Gustavo de Paula <gdepaula@gmail.com>
2024-01-16 11:23:27 -0300

    Form Control

--- meu-primeiro-projeto-latest/src/app/components/forms/reactive-forms/reactive-forms.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/forms/reactive-forms/reactive-forms.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './reactive-forms.component.html',
-  styleUrl: './reactive-forms.component.scss'
+  styleUrl: './reactive-forms.component.scss',
 })
 export class ReactiveFormsComponent {
+  name = new FormControl('');
 
+  public updateName() {
+    this.name.setValue('Maria');
+  }
 }
