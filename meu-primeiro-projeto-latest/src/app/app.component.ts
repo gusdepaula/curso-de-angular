37ae14f5c9b204ad756e0cac9c440355953788d7
Gustavo de Paula <gdepaula@gmail.com>
2024-01-23 10:24:12 -0300

    Host elements

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ContentComponent } from './components/content/content.component';
+import { HostElementsComponent } from './components/host-elements/host-elements.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContentComponent],
+  imports: [CommonModule, RouterOutlet, HostElementsComponent],
   template: `<h1>Curso de Angular</h1>
-    <app-content>
-      <header id="header">
-        <p>Header</p>
-      </header>
-      <p text>Text</p>
-      <p>Text</p>
-      <footer class="footer">
-        <p>Footer</p>
-      </footer>
-    </app-content>`,
+    <app-host-elements />`,
 })
 export class AppComponent {}
