e4cc3a0beeee123a830f5db8d00735795b433ea5
Gustavo de Paula <gdepaula@gmail.com>
2024-01-29 09:47:05 -0300

    OnDestroy + takeUntilDestroyed

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -8,10 +8,11 @@ import { LifeCycleComponent } from './components/life-cycle/life-cycle.component
   standalone: true,
   imports: [CommonModule, RouterOutlet, LifeCycleComponent],
   template: `<h1>Curso de Angular</h1>
-    <app-life-cycle [myNumber]="number">
-      <p #text>Text</p>
-    </app-life-cycle>`,
+    @if(boolean) {<app-life-cycle [myNumber]="number">
+      <p #text>Text</p> </app-life-cycle
+    >} <button (click)="boolean = !boolean">Destroy Component</button>`,
 })
 export class AppComponent {
   public number = 1;
+  public boolean = true;
 }
--- meu-primeiro-projeto-latest/src/app/components/life-cycle/life-cycle.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/life-cycle/life-cycle.component.ts
@@ -10,12 +10,15 @@ import {
   ElementRef,
   Input,
   OnChanges,
+  OnDestroy,
   OnInit,
   SimpleChanges,
   ViewChild,
   signal,
 } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { timer } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-life-cycle',
@@ -32,7 +35,8 @@ export class LifeCycleComponent
     AfterViewInit,
     AfterContentInit,
     AfterContentChecked,
-    AfterViewChecked
+    AfterViewChecked,
+    OnDestroy
 {
   @Input() public myNumber = 0;
   public myText = signal('Gusta');
@@ -40,28 +44,36 @@ export class LifeCycleComponent
   @ViewChild('content') public content!: ElementRef;
   @ContentChild('text') public text!: ElementRef;
 
+  private destroy$ = timer(0, 1000)
+    .pipe(takeUntilDestroyed())
+    .subscribe({
+      next: (next) => console.log('next', next),
+      error: (error) => console.log('error', error),
+      complete: () => console.log('Complete!'),
+    });
+
   // Construtor ou inicializador
   constructor(private fb: FormBuilder) {}
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('ngOnChanges', changes);
+    // console.log('ngOnChanges', changes);
   }
 
   ngOnInit(): void {
-    console.log('ngOnInit');
+    // console.log('ngOnInit');
   }
 
   ngDoCheck(): void {
-    console.log('ngDoCheck');
+    // console.log('ngDoCheck');
   }
 
   ngAfterContentInit(): void {
-    console.log('ngAfterContentInit');
-    console.log(this.text.nativeElement.innerText);
+    // console.log('ngAfterContentInit');
+    // console.log(this.text.nativeElement.innerText);
   }
 
   ngAfterContentChecked(): void {
-    console.log('ngAfterContentChecked');
+    // console.log('ngAfterContentChecked');
   }
 
   ngAfterViewInit(): void {
@@ -70,6 +82,11 @@ export class LifeCycleComponent
   }
 
   ngAfterViewChecked(): void {
-    console.log('ngAfterViewChecked');
+    // console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+    // this.destroy$.unsubscribe();
   }
 }
