4d19c0b8392ceb57bdaff7686477ecc25b509d24
Gustavo de Paula <gdepaula@gmail.com>
2024-02-02 09:46:48 -0300

    Observable + http-Get + Async

--- meu-primeiro-projeto-latest/src/app/components/consume-service/consume-service.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/consume-service/consume-service.component.ts
@@ -21,8 +21,10 @@ export class ConsumeServiceComponent implements OnInit {
 
   public getTask = signal<null | Array<{ id: string; title: string }>>(null);
 
+  public getTask$ = this.#apiService.httpListTask$();
+
   ngOnInit(): void {
-    this.#apiService.httpListTask$().subscribe({
+    this.getTask$.subscribe({
       next: (next) => {
         console.log(next);
       },
--- meu-primeiro-projeto-latest/src/app/services/api.service.ts
+++ meu-primeiro-projeto-latest/src/app/services/api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject, signal } from '@angular/core';
 import { environment } from 'environments/environment';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, shareReplay } from 'rxjs';
 
 interface ITask {
   id: string;
@@ -22,6 +22,6 @@ export class ApiService {
   #url = signal(environment.apiTask);
 
   public httpListTask$(): Observable<ITask[]> {
-    return this.#http.get<ITask[]>(this.#url());
+    return this.#http.get<ITask[]>(this.#url()).pipe(shareReplay());
   }
 }
