f2c788f0c76ac8baf7151ab99b4c434f68ed63f5
Gustavo de Paula <gdepaula@gmail.com>
2024-02-09 10:43:33 -0300

    Navegando entre páginas

--- meu-primeiro-projeto-latest/src/app/pages/home/home.component.ts
+++ meu-primeiro-projeto-latest/src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-hom',
   standalone: true,
-  imports: [],
-  templateUrl: './hom.component.html',
-  styleUrl: './hom.component.scss',
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {}
