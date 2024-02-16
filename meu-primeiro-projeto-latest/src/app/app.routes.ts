9ed55f5ce6fb1b805a1a27eb82934c71c0167a40
Gustavo de Paula <gdepaula@gmail.com>
2024-02-16 11:27:23 -0300

    Lazy load children

--- meu-primeiro-projeto-latest/src/app/app.routes.ts
+++ meu-primeiro-projeto-latest/src/app/app.routes.ts
@@ -3,24 +3,8 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    children: [
-      {
-        path: '',
-        title: 'Home da página',
-        loadComponent: () => import('./pages/home/home.component'),
-      },
-      {
-        path: 'sobre',
-        title: 'Sobre da página',
-        loadComponent: () => import('./pages/sobre/sobre.component'),
-      },
-      {
-        path: 'servicos/:id',
-        title: 'Serviços da página',
-        loadComponent: () =>
-          import('./pages/servicos-prestados/servicos-prestados.component'),
-      },
-    ],
+    loadChildren: () =>
+      import('./pages/curso.routes').then((r) => r.cursoRoutes),
   },
   {
     path: '**',
