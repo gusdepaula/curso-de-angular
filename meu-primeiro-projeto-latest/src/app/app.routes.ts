4f1c1267c6d039ab9376cf607ae210db6a3dde24
Gustavo de Paula <gdepaula@gmail.com>
2024-02-20 11:31:20 -0300

    CanMatch

--- meu-primeiro-projeto-latest/src/app/app.routes.ts
+++ meu-primeiro-projeto-latest/src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { canActivateChildGuard } from './guard/can-activate-child.guard';
+import { canMatchGuard } from './guard/can-match.guard';
 
 export const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
       import('./pages/curso.routes').then((r) => r.cursoRoutes),
-    canActivateChild: [canActivateChildGuard],
+    canMatch: [canMatchGuard],
   },
   {
     path: '**',
