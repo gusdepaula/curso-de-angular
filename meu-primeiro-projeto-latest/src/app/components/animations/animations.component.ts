f3e41758f5a5f2ccd71257ee0a7c0916d2f209c5
Gustavo de Paula <gdepaula@gmail.com>
2024-02-26 16:14:25 -0300

    Trigger + State

--- meu-primeiro-projeto-latest/src/app/components/animations/animations.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/animations/animations.component.ts
@@ -1,3 +1,4 @@
+import { state, style, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
@@ -8,7 +9,18 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
   templateUrl: './animations.component.html',
   styleUrl: './animations.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: [],
+  animations: [
+    trigger('move-ball', [
+      state(
+        'move-left',
+        style({ transform: 'scale(1) translateX(0) rotate(0deg)' })
+      ),
+      state(
+        'move-right',
+        style({ transform: 'scale(0.7) translateX(300px) rotate(360deg)' })
+      ),
+    ]),
+  ],
 })
 export class AnimationsComponent {
   public moveIn = signal('');
