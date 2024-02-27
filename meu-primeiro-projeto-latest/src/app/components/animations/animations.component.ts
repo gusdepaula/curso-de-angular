1a9138195399eb9e95df8196f047713dc1a670d7
Gustavo de Paula <gdepaula@gmail.com>
2024-02-27 10:57:34 -0300

    Transition + if

--- meu-primeiro-projeto-latest/src/app/components/animations/animations.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/animations/animations.component.ts
@@ -20,16 +20,20 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
       state(
         'move-left',
         style({
+          opacity: 0,
           transform: 'scale(1) translateX(0) rotate(0deg)',
         })
       ),
       state(
         'move-right',
         style({
+          opacity: 1,
           transform: 'scale(0.7) translateX(500px) rotate(360deg)',
         })
       ),
       transition('move-left <=> move-right', animate('1s')),
+      transition(':enter', animate('1s')),
+      transition(':leave', animate('2s')),
       transition('* => move-right', animate('5s 1s ease-in-out')),
       transition('* => move-left', animate('1s')),
     ]),
