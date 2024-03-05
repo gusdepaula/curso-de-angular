10162f74d61f6cdd1ea3280176350f66031b2c8b
Gustavo de Paula <gdepaula@gmail.com>
2024-03-05 15:21:34 -0300

    Renderizando no Browser e Server

--- curso-angular-universal-latest/src/app/app.component.ts
+++ curso-angular-universal-latest/src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import {
+  CommonModule,
+  isPlatformBrowser,
+  isPlatformServer,
+} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `<h1>Angular SSR / SSG</h1>`,
 })
 export class AppComponent {
-  title = 'curso-angular-universal-latest';
+  constructor(@Inject(PLATFORM_ID) private plataformId: Object) {
+    if (isPlatformBrowser(this.plataformId)) {
+      console.log(1, localStorage.getItem('value'));
+    }
+
+    if (isPlatformServer(this.plataformId)) {
+      console.log(2, 'Gusta');
+    }
+  }
 }
