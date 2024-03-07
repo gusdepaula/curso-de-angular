c746dba201bed729a2fd1d32257b012d7dcca860
Gustavo de Paula <gdepaula@gmail.com>
2024-03-07 11:46:14 -0300

    Rodando Back-end com Front-end

--- curso-angular-universal-latest/server/modules/hello-world/router.ts
+++ curso-angular-universal-latest/server/modules/hello-world/router.ts
@@ -3,7 +3,7 @@ import { helloWorldController } from './controller/hello-world.controller';
 
 // Router
 const router = Router();
-const baseUrl = 'api/hello-world';
+const baseUrl = '/api/hello-world';
 
 router.get(`${baseUrl}`, helloWorldController.helloWorld);
 
