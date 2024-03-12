4b637b1ad0f0de9fa7a431c4f4725da21be51288
Gustavo de Paula <gdepaula@gmail.com>
2024-03-12 11:22:23 -0300

    @Output() - Parte 2

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -2,14 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<app-output></app-output>`,
+  template: `<ng-template [ngIf]="getDados">
+      <h1>{{ getDados.nome }}</h1>
+      <h2>{{ getDados.idade }}</h2></ng-template
+    ><app-output (enviarDados)="setDados($event)"></app-output>`,
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {}
 
   public addValue: number = 10;
 
+  public getDados: { nome: string; idade: number } | undefined;
+
   public add() {
     this.addValue += 1;
+  }
+
+  public setDados(event: { nome: string; idade: number }) {
+    this.getDados = event;
   }
 }
