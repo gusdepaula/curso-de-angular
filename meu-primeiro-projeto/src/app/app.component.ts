3951cf25f776f9a0f04c2b650f9a2d9f526ed5be
Gustavo de Paula <gdepaula@gmail.com>
2024-03-20 16:36:37 -0300

    Comunicação de componentes com Service

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<app-food-list></app-food-list>`,
+  template: `<app-food-add></app-food-add><app-food-list></app-food-list>`,
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {}
