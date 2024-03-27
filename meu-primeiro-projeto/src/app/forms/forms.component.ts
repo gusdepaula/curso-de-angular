2a24a263bba50b41150b7f0306f07a30310d9ff2
Gustavo de Paula <gdepaula@gmail.com>
2024-03-27 10:01:46 -0300

    ngForm + Selected

--- meu-primeiro-projeto/src/app/forms/forms.component.ts
+++ meu-primeiro-projeto/src/app/forms/forms.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.scss']
+  styleUrls: ['./forms.component.scss'],
 })
 export class FormsComponent {
-
+  public listComida: Array<{ comida: string; preco: string }> = [
+    { comida: 'X-Salada', preco: 'R$10,00' },
+    { comida: 'X-Egg', preco: 'R$13,00' },
+    { comida: 'X-Bacon', preco: 'R$15,00' },
+  ];
 }
