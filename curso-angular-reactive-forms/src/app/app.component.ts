925b1bc82601e209d40f24eed0d34db8bd5aa59b
Gustavo de Paula <gdepaula@gmail.com>
2024-04-16 15:18:14 -0300

    Utilizando o Formulário

--- curso-angular-reactive-forms/src/app/app.component.ts
+++ curso-angular-reactive-forms/src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: `<router-outlet></router-outlet>`,
+  template: `<router-outlet></router-outlet>`
 })
-export class AppComponent {}
+export class AppComponent {
+}
