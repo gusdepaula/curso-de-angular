96d0e41a558bc64b8599fbef9468596652d1aa0b
Gustavo de Paula <gdepaula@gmail.com>
2024-04-18 15:45:50 -0300

    Criando Rotas

--- routes/src/app/app.routes.ts
+++ routes/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
 
 export const routes: Routes = [
-  { path: '', component: PageHomeComponent, pathMatch: 'full' },
-  { path: 'sobre', component: PageSobreComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'sobre', component: SobreComponent },
+  { path: '404', component: PageErrorComponent },
 ];
