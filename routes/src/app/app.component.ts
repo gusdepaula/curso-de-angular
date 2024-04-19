a8bd5406f7d7a0405e85b852c636a26db9f3ac12
Gustavo de Paula <gdepaula@gmail.com>
2024-04-19 13:22:03 -0300

    Navegação entre páginas

--- routes/src/app/app.component.ts
+++ routes/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: `<router-outlet />`,
+  imports: [CommonModule, RouterOutlet, MenuComponent],
+  template: `<app-menu /><router-outlet />`,
 })
 export class AppComponent {
   title = 'routes';
---
+++ routes/src/app/shared/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss',
+})
+export class MenuComponent {}
