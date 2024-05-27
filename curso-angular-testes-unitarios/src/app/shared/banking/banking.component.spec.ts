24edf2b156240ed1573d0d4c3061a7b694372eef
Gustavo de Paula <gdepaula@gmail.com>
2024-05-27 10:15:53 -0300

    Testando a interface do componente

--- curso-angular-testes-unitarios/src/app/shared/banking/banking.component.spec.ts
+++ curso-angular-testes-unitarios/src/app/shared/banking/banking.component.spec.ts
@@ -35,6 +35,15 @@ describe('BankingComponent', () => {
     expect(component.getCarteira).toEqual(60);
   });
 
+  it(`(I) setSacar(): shoud transfer poupanca from carteira`, () => {
+    let el = fixture.debugElement.nativeElement;
+
+    el.querySelector('#input-sacar').value = '10';
+    el.querySelector('#sacar').click();
+    fixture.detectChanges();
+    expect(el.querySelector('#get-carteira').textContent).toEqual('60');
+  });
+
   it(`(U) setSacar(): should transfer poupanca dont have string (isNaN) or poupanca < value`, () => {
     expect(component.setSacar('string')).not.toBeTruthy();
     expect(component.setSacar('100')).not.toBeTruthy();
@@ -56,5 +65,14 @@ describe('BankingComponent', () => {
 
     expect(component.getPoupanca).toEqual(10);
     expect(component.getCarteira).toEqual(50);
+  });
+
+  it(`(I) setDepositar(): should transfer carteira from poupanca`, () => {
+    let el = fixture.debugElement.nativeElement;
+
+    el.querySelector('#input-depositar').value = '10';
+    el.querySelector('#depositar').click();
+    fixture.detectChanges();
+    expect(el.querySelector('#get-poupanca').textContent).toEqual('20');
   });
 });
