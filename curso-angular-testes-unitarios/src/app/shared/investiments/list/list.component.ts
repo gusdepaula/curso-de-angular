6e509ee163e367e9eb37f7a18d45c77fb7fda5c7
Gustavo de Paula <gdepaula@gmail.com>
2024-05-27 10:47:33 -0300

    Criando component investiments

---
+++ curso-angular-testes-unitarios/src/app/shared/investiments/list/list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Investiments } from '../model/investiments';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss',
+})
+export class ListComponent {
+  public investiments: Array<Investiments> = [
+    { name: 'itaú', value: 100 },
+    { name: 'bb', value: 100 },
+    { name: 'nubank', value: 100 },
+    { name: 'inter', value: 100 },
+  ];
+}
