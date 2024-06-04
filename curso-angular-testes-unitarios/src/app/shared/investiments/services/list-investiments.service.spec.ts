d19b20b0515821ba0d9fc7e46dfcc52efc2a06a6
Gustavo de Paula <gdepaula@gmail.com>
2024-06-04 10:27:19 -0300

    Configurando Test do serviço

--- curso-angular-testes-unitarios/src/app/shared/investiments/services/list-investiments.service.spec.ts
+++ curso-angular-testes-unitarios/src/app/shared/investiments/services/list-investiments.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 import { ListInvestimentsService } from './list-investiments.service';
 
 describe('ListInvestimentsService', () => {
   let service: ListInvestimentsService;
+  let httpTestingController: HttpTestingController;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ListInvestimentsService);
   });
 
