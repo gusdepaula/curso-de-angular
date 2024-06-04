c57e00ae395059b6eea960b4b7e7e04add54794e
Gustavo de Paula <gdepaula@gmail.com>
2024-06-04 11:29:44 -0300

    Configurando Test do serviço 2

--- curso-angular-testes-unitarios/src/app/shared/investiments/services/list-investiments.service.spec.ts
+++ curso-angular-testes-unitarios/src/app/shared/investiments/services/list-investiments.service.spec.ts
@@ -5,12 +5,38 @@ import {
 } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
 import { ListInvestimentsService } from './list-investiments.service';
+import { Investiments } from '../model/investiments';
 
 describe('ListInvestimentsService', () => {
   let service: ListInvestimentsService;
   let httpTestingController: HttpTestingController;
   let httpClient: HttpClient;
 
+  const URL =
+    'https://raw.githubusercontent.com/troquatte/fake-server/main/investiments-all.json';
+
+  const mockList: Array<Investiments> = [
+    {
+      name: 'Banco 1',
+      value: 100,
+    },
+    {
+      name: 'Banco 2',
+      value: 100,
+    },
+    {
+      name: 'Banco 3',
+      value: 100,
+    },
+    {
+      name: 'Banco 4',
+      value: 100,
+    },
+    {
+      name: 'Banco 5',
+      value: 100,
+    },
+  ];
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -18,6 +44,10 @@ describe('ListInvestimentsService', () => {
     httpClient = TestBed.inject(HttpClient);
     httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ListInvestimentsService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
